7b0f3ab72bd28719d1d54501f75810737e545084
Jules-G-code <jules.gautier@gmail.com>
2026-01-26 12:35:07 +0100

    hacheur non fonctionel

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,16 @@ int BP2 = 12;
 // declaration du potentiometre
 int pot = 33;
 
+// declaration du PWM
+int PWM = 27;
+int phase = 26;
+int ON_PWM = 25;
+
+// caracteristique de la PWM
+int freq = 25000;
+int canal0 = 0;
+int resolution = 11;
+
 int Val_BP0;
 int Val_BP1;
 int Val_BP2;
@@ -31,10 +41,20 @@ void setup()
   pinMode(BP1, INPUT_PULLUP);
   pinMode(BP2, INPUT_PULLUP);
 
+  pinMode(PWM, OUTPUT);
+  pinMode(phase, OUTPUT);
+  pinMode(ON_PWM, OUTPUT);
+
   // Initialise l'écran LCD
   Wire1.setPins(15, 5);
   lcd.begin(16, 2, LCD_5x8DOTS, Wire1);
   lcd.setRGB(colorR, colorG, colorB);
+
+  // config PWM
+  ledcSetup(canal0, freq, resolution);
+
+  // liaison du canal du PWM avec les broches de l'ESP32
+  ledcAttachPin(PWM, canal0);
 }
 
 void loop()
@@ -53,4 +73,9 @@ void loop()
   lcd.setCursor(0, 0);
   lcd.printf("potar = %d", Val_pot);
   delay(300);
+
+  // ecriture rapport cyclique 
+  //digitalWrite(phase, BP1);
+  //digitalWrite(ON_PWM, 0);
+  ledcWrite(canal0, 1024);
 }
