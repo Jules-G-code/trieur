68c42f0e9fb44d0790cbe3a5afcb1a5657d362b8
Jules_G_code <jules.gautier@gmail.com>
2026-01-30 12:36:42 +0100

    switch etat + asservissement qui ne fonctionne pas

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
 #include "rgb_lcd.h"
+#include <ESP32Encoder.h>
+
+void vTaskPeriodic(void *pvParameters);
 
 rgb_lcd lcd;
 
@@ -10,6 +13,18 @@ int BP2 = 12;
 
 // declaration du potentiometre
 int pot = 33;
+
+// Déclaration du capteur IR
+int IR0 = 36; // broche 36
+int Val_IR0;
+
+// Déclaration du codeur
+ESP32Encoder encoder;
+long encoder_count = 0;
+long step_count = 0;
+
+// Déclaration de la machine a état
+int etat = 1;
 
 // declaration du PWM
 int PWM = 27;
@@ -26,10 +41,15 @@ int Val_BP1;
 int Val_BP2;
 int Val_pot;
 
+// Seuil de détection du capteur IR (à ajuster selon votre capteur)
+int IR_Seuil = 2000;
+
 // Paramétrage du rétroéclairage
-const int colorR = 255;
-const int colorG = 0;
-const int colorB = 0;
+int colorR = 255;
+int colorG = 0;
+int colorB = 0;
+
+int csg_vitesse;
 
 void setup()
 {
@@ -45,6 +65,10 @@ void setup()
   pinMode(phase, OUTPUT);
   pinMode(ON_PWM, OUTPUT);
 
+  // Utilise attachFullQuad pour lire tous les changements
+  encoder.attachFullQuad(19, 23);
+  encoder.setCount(0);
+
   // Initialise l'écran LCD
   Wire1.setPins(15, 5);
   lcd.begin(16, 2, LCD_5x8DOTS, Wire1);
@@ -55,6 +79,12 @@ void setup()
 
   // liaison du canal du PWM avec les broches de l'ESP32
   ledcAttachPin(PWM, canal0);
+
+  Serial.begin(115200);
+  Serial.printf("Initialisations\n");
+
+  // Création de la tâche périodique
+  xTaskCreate(vTaskPeriodic, "vTaskPeriodic", 10000, NULL, 2, NULL);
 }
 
 void loop()
@@ -66,16 +96,132 @@ void loop()
 
   Val_pot = analogRead(pot);
 
+  // Lecture de la valeur du capteur IR
+  Val_IR0 = analogRead(IR0);
+
+  // Lecture de la valeur du codeur
+  encoder_count = encoder.getCount();
+
   // Message dans le terminal
-  Serial.printf("bp0=%d  bp1=%d\n bp2=%d", Val_BP0, Val_BP1, Val_BP2);
+  // Serial.printf("bp0=%d  bp1=%d  bp2=%d  IR0=%d  encoder=%ld  etat=%d\n", Val_BP0, Val_BP1, Val_BP2, Val_IR0, encoder_count, etat);
 
   // Affichage sur l'écran LCD
   lcd.setCursor(0, 0);
-  lcd.printf("potar = %d", Val_pot);
-  delay(300);
+  lcd.printf("IR0=%d", Val_IR0);
+  lcd.setCursor(0, 1);
+  lcd.printf("enc=%ld  etat=%d", encoder_count, etat);
 
-  // ecriture rapport cyclique 
-  //digitalWrite(phase, BP1);
-  //digitalWrite(ON_PWM, 0);
-  ledcWrite(canal0, 1024);
+
+  switch (etat)
+  {
+  case 1:
+    digitalWrite(phase, 0);
+    digitalWrite(ON_PWM, 1);
+    ledcWrite(canal0, 600);
+    if (Val_IR0 > IR_Seuil)
+    {
+      etat = 2;
+    }
+    break;
+
+  case 2:
+    encoder.setCount(0);
+    encoder_count = 0;
+    step_count = 0;
+    digitalWrite(ON_PWM, 0);
+    ledcWrite(canal0, 0);
+    if (Val_BP0 == 0)
+    {
+      step_count = 100;
+      etat = 3;
+    }
+    else if (Val_BP1 == 0)
+    {
+      step_count = -100;
+      etat = 5;
+    }
+    break;
+
+  case 3:
+    digitalWrite(phase, 0);
+    digitalWrite(ON_PWM, 1);
+    ledcWrite(canal0, 600);
+
+    if (encoder.getCount() > step_count)
+    {
+      etat = 4;
+    }
+    break;
+
+  case 4:
+    digitalWrite(ON_PWM, 0);
+    ledcWrite(canal0, 0);
+    step_count += 100;
+    delay(1000);
+    etat = 3;
+
+    if (step_count > 840)
+    {
+      etat = 2;
+    }
+    break;
+
+  case 5:
+    digitalWrite(phase, 1);
+    digitalWrite(ON_PWM, 1);
+    ledcWrite(canal0, 600);
+    if (encoder.getCount() < step_count)
+    {
+      etat = 6;
+    }
+    break;
+
+  case 6:
+    digitalWrite(ON_PWM, 0);
+    ledcWrite(canal0, 0);
+
+    delay(1000);
+    step_count -= 100;
+    etat = 5;
+
+    if (step_count < -840)
+    {
+      etat = 2;
+    }
+    break;
+  }
+}
+
+void vTaskPeriodic(void *pvParameters)
+{
+  int encoder_memo=0;
+  int image_vitesse;
+  long encoder_count;
+  int erreur;
+  int commande;
+  TickType_t xLastWakeTime;
+  // Lecture du nombre de ticks quand la tâche commence
+  xLastWakeTime = xTaskGetTickCount();
+  while (1)
+  {
+    Serial.printf("A répéter\n");
+    encoder_count = encoder.getCount(); 
+    image_vitesse = encoder_count - encoder_memo;
+    encoder_memo = encoder_count;
+
+    erreur = csg_vitesse - image_vitesse;
+
+    // Asservissement proportionnel
+    commande = 550 + (erreur * 5);   // 5 est le gain proportionnel à ajuster
+    commande = constrain(commande, 0, 2047);  // Limite entre 0 et 2047 (résolution 11 bits)
+    ledcWrite(canal0, commande);
+
+    Serial.printf("image_vitesse=%d  erreur=%d  commande=%d\n", image_vitesse, erreur, commande);
+
+    // Endort la tâche pendant le temps restant par rapport au réveil,      
+    // ici 100ms, donc la tâche s'effectue ici toutes les 100ms.
+    // xLastWakeTime sera mis à jour avec le nombre de ticks au prochain
+    // réveil de la tâche.
+    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(10));
+  }
 }
